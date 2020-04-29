3a0439e5b1e1d4f973a6d3fbc6c6b36fe0478d44
mickjosh <felix_roux@hotmail.com>
2020-04-29 07:25:18 -0400

    Adding The Function Scheduler

---
+++ src/main/java/frc/robot/Interface/Action.java
@@ -0,0 +1,9 @@
+package frc.robot.Interface;
+
+/**
+ * The interface for function to be used as action
+ */
+public interface Action 
+{
+    public void Invoke();
+}
---
+++ src/main/java/frc/robot/Interface/Condition.java
@@ -0,0 +1,10 @@
+
+package frc.robot.Interface;
+
+/**
+ * The interface for function to be used as condition
+ */
+public interface Condition
+{
+    public boolean Invoke();
+}
